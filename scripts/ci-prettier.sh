0c69a76093cb7f8b047365935792842cfc9144e8
anvie <obin.mf@gmail.com>
2021-04-18 20:17:05 +0700

    fixup wrong repo url

--- scripts/ci-prettier.sh
+++ scripts/ci-prettier.sh
@@ -7,7 +7,7 @@ then
     git config --global user.name "Nuchain Wiki CI"
     echo "machine github.com login nusantarachain password $GITHUB_BOT_TOKEN" > ~/.netrc
     node_modules/.bin/prettier -c docs/*.md --write
-    git remote set-url origin https://github.com/nusantarachain/nuchain-wiki
+    git remote set-url origin https://github.com/nusantarachain/wiki
     ssh-add -D
     ssh-add ~/.ssh/id_rsa_93218110749961b89a5041c875703b1d
     git add -A; git commit -m "Ran prettier"; git push -u origin HEAD;
